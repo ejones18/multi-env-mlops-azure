263749e569cddc1db589b132298abfb538298e0d
Ethan Jones <jonesethan@microsoft.com>
2024-06-14 22:20:07 +0100

    Update register.py to support multi-environment model registering

--- data-science/src/register/register.py
+++ data-science/src/register/register.py
@@ -1,20 +1,22 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 """
-Registers trained ML model if deploy flag is True.
+Registers trained ML model and shares to an AZure ML workspace.
+
+Lastest update: 14/06/24
 """
 
 import argparse
 from pathlib import Path
-import pickle
-import mlflow
-
 import os 
 import json
 
-def parse_args():
-    '''Parse input arguments'''
+import mlflow
+from azure.ai.ml import MLClient
+from azure.identity import ManagedIdentityCredential
 
+def parse_args():
+    """Parse command line arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', type=str, help='Name under which model will be registered')
     parser.add_argument('--model_path', type=str, help='Model directory')
@@ -24,12 +26,11 @@ def parse_args():
     )
     args, _ = parser.parse_known_args()
     print(f'Arguments: {args}')
-
     return args
 
 
 def main(args):
-    '''Loads model, registers it if deply flag is True'''
+    """Loads model, registers it to the model registry then shares to a shared Azure ML registry."""
 
     with open((Path(args.evaluation_output) / "deploy_flag"), 'rb') as infile:
         deploy_flag = int(infile.read())
@@ -58,28 +59,31 @@ def main(args):
         output_path = os.path.join(args.model_info_output_path, "model_info.json")
         with open(output_path, "w") as of:
             json.dump(dict, fp=of)
+        
+        # write model to shared registry
+        credential = ManagedIdentityCredential(client_id="") # Managed identity client ID with Azure ML Registry User role applied
+        ml_client_workspace = MLClient(credential=credential,
+                                       subscription_id = "",
+                                       resource_group_name = "",
+                                       workspace_name = "")
+        ml_client_workspace.models.share(name=args.model_name, 
+                                         version=str(model_version), 
+                                         registry_name="", 
+                                         share_with_name=args.model_name,
+                                         share_with_version=str(model_version))
 
     else:
         print("Model will not be registered!")
 
 if __name__ == "__main__":
-
     mlflow.start_run()
-    
-    # ---------- Parse Arguments ----------- #
-    # -------------------------------------- #
-
     args = parse_args()
-    
     lines = [
         f"Model name: {args.model_name}",
         f"Model path: {args.model_path}",
         f"Evaluation output path: {args.evaluation_output}",
     ]
-
     for line in lines:
         print(line)
-
     main(args)
-
     mlflow.end_run()
